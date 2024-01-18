7f94a72975e04dc9ff721690878f2c6725e78947
Calvin Wong <calviw8@uci.edu>
2024-01-17 16:47:28 -0800

    Updated a1p1.py
    Updated the program_command() function and added the get_given(user_input, L_option) function.

--- a1p1.py
+++ a1p1.py
@@ -19,7 +19,9 @@ def show_files_only(a_path):
     for content in directory_contents:
         each_path = a_path + os.path.sep + content
         if os.path.isfile(each_path):
+            # print(content)
             print(each_path)
+
 
 def show_files_by_extension(a_path, file_type):
     directory_contents = os.listdir(a_path)
@@ -39,20 +41,48 @@ def show_files_by_name(a_path, file_name):
                 print(each_path)
 
 
-def program_command():
-    pass
+def get_given(user_input, L_option):
+    the_option = user_input.find(L_option)
+    given = user_input[(the_option+3):]
+    print(given)
+    return given
 
+
+def program_command():
+    while True:
+        user_input = input()
+        user_path = user_input.split()
+
+        if user_path[0] == 'Q':
+            break
+        elif user_path[0] == 'L' and len(user_path) > 1:
+            if len(user_path) == 2:
+                show_files_then_directories(user_path[1])
+                print()
+            elif user_path[2] == '-f':
+                show_files_only(user_path[1])
+                print()
+            elif user_path[2] == '-s':
+                name = get_given(user_input, '-s')
+                show_files_by_name(user_path[1], name)
+                print()
+            elif user_path[2] == '-e':
+                show_files_by_extension(user_path[1], user_path[3])
+                print()
+        
 
 def main():
-    user_input = 'Q C:\\Users\\cowvy\\OneDrive\\UCI\\Writing'
+    program_command()
+    # user_input = 'L C:\\Users\\cowvy\\OneDrive\\UCI\\Writing -s GA final draft guide.pdf'
     # user_input = input()
-    path = user_input.split()
+    # path = user_input.split()
     # show_files_then_directories(path[1])
     # print()
-    show_files_only(path[1])
+    # show_files_only(path[1])
     # print()
     # show_files_by_name(path[1], "GA final draft guide.pdf")
     # print()
     # show_files_by_extension(path[1], "pdf")
+
 
 main()
